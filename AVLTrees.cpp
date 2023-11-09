654fcabffca48e412f9da87428623fe7ed065374
Varun Gurnani <81065122+varungurnani@users.noreply.github.com>
2023-11-09 12:22:56 +0530

    Update AVLTrees.cpp

--- AVLTrees.cpp
+++ AVLTrees.cpp
@@ -286,3 +286,20 @@ public:
         }
     }
 };
+
+/////////////////////////////////////////   main function     //////////////////////////////
+
+int main() {
+    AVLTree<int> tree;
+
+    tree.insert(10);
+    tree.insert(20);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(50);
+    tree.insert(25);
+
+    tree.levelOrderTraversal();
+
+    return 0;
+}
